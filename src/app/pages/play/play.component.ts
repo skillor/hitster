443a52ee30bdf726eeb2973e0cd083782f93e5e4
skillor <skillor@gmx.net>
2024-02-16 01:38:46 +0100

    Update play.component.ts

--- src/app/pages/play/play.component.ts
+++ src/app/pages/play/play.component.ts
@@ -109,7 +109,7 @@ export class PlayComponent implements OnInit {
   }
 
   closeMenu() {
-    // if (this.isMobile) this.requestFullscreen();
+    if (this.isMobile) this.requestFullscreen();
     this.menuModal = false;
   }
 
