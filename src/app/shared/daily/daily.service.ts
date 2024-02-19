c6d94f7631fd42d7021ca18893461a9161642f40
skillor <skillor@gmx.net>
2024-02-19 21:34:55 +0100

    fix daily value

--- src/app/shared/daily/daily.service.ts
+++ src/app/shared/daily/daily.service.ts
@@ -27,7 +27,7 @@ export class DailyService {
 
   getDailyValue(): string | null {
     const dailyValue = localStorage.getItem('daily_value');
-    if (dailyValue && matchingDate(new Date(getDayString()), new Date(dailyValue))) return dailyValue;
+    if (dailyValue && matchingDate(new Date(getDayString()), new Date(dailyValue.split(" ")[0]))) return dailyValue;
     return null;
   }
 
